c600762fb816ea26b3450219a639921ad2f3e18e
kimyuna <dbskqnsghd@naver.com>
2023-02-08 18:20:15 +0900

    ProductAll page style

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const ProductCard = ({item}) => {
   return (
-    <div>
-      <img width={300} src={item?.img}/>
-      <div>{item?.choice == true? "Conscious Choice" : ""}</div>
-      <div>{item?.title}</div>
-      <div>{item?.price}</div>
-      <div>{item?.new == true? "신제품" : ""}</div>
+    <div className='card-area'>
+      <div className='product-card'>
+        <img width={300} src={item?.img}/>
+        <div>{item?.choice == true? "Conscious Choice" : ""}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new == true? "신제품" : ""}</div>
+    </div>
     </div>
   )
 }
